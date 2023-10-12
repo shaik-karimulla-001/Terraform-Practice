1e691255dee075586cb4ee5ed65f741d326377e0
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-12 16:16:59 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,63 @@
+resource "aws_s3_bucket" "my-s3-bucket" {
+    bucket_prefix = var.bucket_prefix
+    acl = var.acl
+    
+     versioning {
+      enabled = var.versioning
+    }
+    
+    tags = var.tags
+  }
+
+  
+  variable "aws_region" {
+    description = "The AWS region to create things in." 
+    default     = "ap-south-1" 
+}
+
+variable "key_name" { 
+ description = " SSH keys to connect to ec2 instance" 
+ default     =  "karim" 
+}
+
+variable "instance_type" { 
+ description = "instance type for ec2" 
+ default     =  "t2.micro" 
+}
+
+variable "security_group" { 
+ description = "Name of security group" 
+ default     = "terraform-sgroup-dec-2021" 
+}
+
+variable "tag_name" { 
+ description = "Tag Name of for Ec2 instance" 
+ default     = "my-ec2-instance" 
+} 
+variable "ami_id" { 
+ description = "AMI for Ubuntu Ec2 instance" 
+ default     = "ami-0a5ac53f63249fba0" 
+}
+variable "versioning" {
+ type        = bool
+ description = "(Optional) A state of versioning."
+ default     = true
+}
+variable "acl" {
+ type        = string
+ description = " Defaults to private "
+ default     = "private"
+}
+variable "bucket_prefix" {
+ type        = string
+ description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+ default     = "my-s3bucket-"
+}
+variable "tags" {
+ type        = map
+ description = "(Optional) A mapping of tags to assign to the bucket."
+ default     = {
+     environment = "DEV"
+     terraform   = "true"
+ }
+}
