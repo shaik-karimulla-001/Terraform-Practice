baac63e58fc6a34e16a5effaa9dbe880e07963de
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-12 17:17:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-for-tf-state"
+    bucket = "my-tf-state-bckup"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-db-table-tf"
