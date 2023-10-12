42c5628eb58eb47a47d2088f4cd1853c13857c3a
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-12 17:20:52 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,12 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
-    bucket_prefix = var.bucket_prefix
-    acl = var.acl
+  bucket_prefix = var.bucket_prefix
+  acl = var.acl
+  
+   versioning {
+    enabled = var.versioning
+  }
+  
+  tags = var.tags
+}
     
-     versioning {
-      enabled = var.versioning
-    }
     
