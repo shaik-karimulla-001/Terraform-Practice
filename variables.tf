40e33b012dac2bda916e34eac9570bf776863b8c
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-12 17:12:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "security_group" {
 
 variable "tag_name" { 
  description = "Tag Name of for Ec2 instance" 
- default     = "my-ec2-instance" 
+ default     = "myfirst-Terraform-Instance" 
 } 
 variable "ami_id" { 
  description = "AMI for Ubuntu Ec2 instance" 
