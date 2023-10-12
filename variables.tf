10373c928abe02cf2c7444b3ab2f5d15a31002c0
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-12 17:37:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
  type        = string
  description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
- default     = "my-tokkalo-bucket-"
+ default     = "my-tokkalo-bucket"
 }
 variable "tags" {
  type        = map
