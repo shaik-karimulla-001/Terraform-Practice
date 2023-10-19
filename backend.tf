4ccea872bb47c64faa7e2aa1aa4890eff0a37d5f
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-19 12:45:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-tokkalo-bucket-for-tf-state"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "new-dynamo-db-table-tf"
   }
 }
