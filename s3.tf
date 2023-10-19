968828499ae32cbe7f702ec28afb4bf01aaa30db
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-10-19 13:41:09 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "my-tokkalo-bucket" {
-  bucket = "my-tokkalo-bucket"  # Replace with your desired bucket name
+ bucket_prefix = var.bucket_prefix
   acl = var.acl
-
-  versioning {
+  
+   versioning {
     enabled = var.versioning
   }
-
+  
   tags = var.tags
 }
