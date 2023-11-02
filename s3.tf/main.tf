93b58fd01dfdac0efe4da99b4d6ef0c9c74085a8
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-02 15:11:57 +0530

    Update main.tf

--- s3.tf/main.tf
+++ s3.tf/main.tf
@@ -1,1 +1,8 @@
+provider "aws" {
+  region = "us-east-1"
+  # Add your AWS access and secret keys here, or use an AWS profile
+}
 
+resource "aws_s3_bucket" "marathali-bucket" {
+  bucket = "marathali-bucket"
+}
