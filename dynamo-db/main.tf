63bb4e645a7e1d00abfead600b49a91aa9c59b97
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-02 16:07:13 +0530

    Update main.tf

--- dynamo-db/main.tf
+++ dynamo-db/main.tf
@@ -1,1 +1,24 @@
 
+# Create the DynamoDB table
+resource "aws_dynamodb_table" "new-dynamodb-table" {
+  name           = "new-dynamodb-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "my-apple-key"
+  range_key      = "my-android-key"
+
+  attribute {
+    name = "my-apple-key"
+    type = "S"
+  }
+
+  attribute {
+    name = "my-android-key"
+    type = "N"
+  }
+
+  # Define the provisioned throughput, if using "PROVISIONED" billing mode
+  # provisioned_throughput {
+  #   read_capacity  = 5
+  #   write_capacity = 5
+  # }
+}
