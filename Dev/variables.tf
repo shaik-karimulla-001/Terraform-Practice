04c59bf306e1cc504706557e32174e5fc18449bd
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-03 18:37:20 +0530

    Create variables.tf

---
+++ Dev/variables.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "./ec2_instance"
+}
+module "aws_dynamodb_table" {
+  source = "./dynamo-db"
+}
+
+module "s3-bucket" {
+  source = "./s3.tf"
+}
