289b442db5cb304d818c59e78c5e7919fa09a791
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 09:52:43 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
    type = string
-  default    = "ami-05c13eab67c5d8861"
+  default    = "ami-0fc5d935ebf8bc3bc"
 }
 
 variable "instance_type" {
