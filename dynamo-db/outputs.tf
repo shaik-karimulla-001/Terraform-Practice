d44aafd5cb2d39a6faf75e2c157fd082e0ef0aed
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 13:16:09 +0530

    Update outputs.tf

--- dynamo-db/outputs.tf
+++ dynamo-db/outputs.tf
@@ -1,7 +1,7 @@
 # Define an output to expose the DynamoDB table name
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.new-dynamodb-table.name
+  value = aws_dynamodb_table.new-dynamodb-table1.name
 }
 output "dynamodb_table_arn" {
-  value = aws_dynamodb_table.new-dynamodb-table.arn
+  value = aws_dynamodb_table.new-dynamodb-table1.arn
 }
