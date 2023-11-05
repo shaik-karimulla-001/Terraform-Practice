05ecd4b7ddcf6cc1657ceed785d3a4b1f0f9f93e
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 10:17:22 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = var.aws_region
+  region = "var.aws_region"
 }
 
 resource "aws_instance" "example" {
-  ami        = var.ami_id
-  instance_type = var.instance_type
+  ami        = "var.ami_id"
+  instance_type = "var.instance_type"
 }
