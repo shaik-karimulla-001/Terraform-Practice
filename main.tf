80f6899749ffdcd7022803243ec5551fdf4132ac
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 09:33:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,9 +52,7 @@ resource "aws_instance" "yellow-instance" {
   key_name      = "var.key_name"
   instance_type = "var.instance_type"
   vpc_security_group_ids = [aws_security_group.super-secure.id]
-  tags = {
-    Name = var.tag_name
-  }
+  
 }
 
 # Create the DynamoDB table
