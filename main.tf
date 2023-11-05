5af15b6d5684e0e37bbab4ff8e6f5c45f3305a64
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 09:48:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "super-secure" {
 }
 
 # Create an EC2 instance
-resource "aws_instance" "yellow-instance" {
+resource "aws_instance" "yellow_instance" {
   ami           = "var.ami_id"
   key_name      = "var.key_name"
   instance_type = "var.instance_type"
