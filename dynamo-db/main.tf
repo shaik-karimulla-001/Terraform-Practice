65e8dfd9cef632bc4eb42dbae0797d2e2c5763b3
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-05 13:15:15 +0530

    Update main.tf

--- dynamo-db/main.tf
+++ dynamo-db/main.tf
@@ -1,7 +1,6 @@
-
 # Create the DynamoDB table
-resource "aws_dynamodb_table" "my-dynamodb-table" {
-  name           = "my-dynamodb-table"
+resource "aws_dynamodb_table" "my-dynamodb-table1" {
+  name           = "my-dynamodb-table1"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "my-apple-key"
   range_key      = "my-android-key"
