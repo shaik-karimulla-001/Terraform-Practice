736869899a54887aaa6ef045e109d3a891761ad8
shaik-karimulla-001 <103804777+shaik-karimulla-001@users.noreply.github.com>
2023-11-06 11:49:59 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,19 +1,15 @@
 variable "aws_region" {
   description = "The AWS region to use"
-  default     = "ap-south-1"
 }
 
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance"
-  default    = "ami-02e94b011299ef128"
 }
 
 variable "instance_type" {
   description = "The EC2 instance type"
-  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "SSH keys to connect to ec2 instance"
-  default     = "karim"
 }
